34ea54ea88aebf50696a4e0b9193533dc31608d5
Joseph Lee <>
2020-12-21 23:08:26 -0500

    add hot update for running streamlist procs by setting pythonpath

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,4 +3,5 @@ STREAM_DEFAULT=ch2_bandits.py
 export PYENV_VERSION=rlbook
 # nohup jupyter lab &> log.jupyter&
 echo streamlit run $STREAM_DEFAULT
+export PYTHONPATH=~/projects/rlbook/
 streamlit run streamlit/$STREAM_DEFAULT
