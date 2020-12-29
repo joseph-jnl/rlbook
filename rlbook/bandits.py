c39c28f5bb29e61e1b91305bfff24225476c4acb
Joseph Lee <>
2020-12-28 23:05:26 -0500

    run black

--- rlbook/bandits.py
+++ rlbook/bandits.py
@@ -8,17 +8,17 @@ import secrets
 
 
 def init_zero(testbed):
-    """
-    """
+    """"""
     return {a: 0 for a in testbed.expected_values}
-    
+
+
 def init_optimistic(testbed):
-    """
-    """
+    """"""
     op_val = abs(max([testbed.action_value(a) for a in testbed.expected_values])) * 5
     testbed.reset_ev()
 
     return {a: int(op_val) for a in testbed.expected_values}
+
 
 class Bandit(metaclass=ABCMeta):
     """Base Bandit class
@@ -26,24 +26,27 @@ class Bandit(metaclass=ABCMeta):
     Attributes:
         testbed (TestBed class object):
             Testbed object that returns a Reward value for a given Action
-        columns (list of strings): 
+        columns (list of strings):
             List of numpy column names to use when outputting results
             as a pandas dataframe.
-        action_values (numpy array): 
-            Stores results of the actions values method. 
+        action_values (numpy array):
+            Stores results of the actions values method.
             Contains Run, Step, Action, and Reward
             Initialized as None, and created with the run method.
         Q_init (initialization function):
             Function to use for initializing Q values, defaults to zero init
     """
-    
-    def __init__(self, testbed: Type[Testbed], 
-            Q_init: Callable = init_zero
-            ):
+
+    def __init__(self, testbed: Type[Testbed], Q_init: Callable = init_zero):
         self.testbed = testbed
-        self.columns = ['Run', 'Step', 'Action', 'Reward',]
+        self.columns = [
+            "Run",
+            "Step",
+            "Action",
+            "Reward",
+        ]
         self.action_values = None
-        self.Q_init = Q_init 
+        self.Q_init = Q_init
         self.initialization()
 
     def initialization(self):
@@ -53,27 +56,24 @@ class Bandit(metaclass=ABCMeta):
         self.At = self.argmax(self.Q)
 
     def argmax(self, Q):
-        """Return max estimate Q, if tie between actions, choose at random between tied actions
-        """
+        """Return max estimate Q, if tie between actions, choose at random between tied actions"""
         Q_array = np.array(list(self.Q.values()))
         At = np.argwhere(Q_array == np.max(Q_array)).flatten().tolist()
-        
+
         if len(At) > 1:
             At = np.random.choice(At)
         else:
             At = At[0]
-        
+
         return list(Q.keys())[At]
 
     @abstractmethod
     def select_action(self):
-        """Select action logic
-        """
+        """Select action logic"""
         pass
 
     def run(self, steps, n_runs=1, n_jobs=4, serial=False):
-        """Run bandit for specified number of steps and optionally multiple runs
-        """
+        """Run bandit for specified number of steps and optionally multiple runs"""
 
         if serial:
             self.action_values = self._serialrun(steps, n_runs)
@@ -112,32 +112,33 @@ class Bandit(metaclass=ABCMeta):
 
     def _multirun(self, steps, n_runs, n_jobs=4):
         with ProcessPoolExecutor(max_workers=n_jobs) as executor:
-            action_values = executor.map(self._singlerun, [steps for n in range(n_runs)], list(range(n_runs)))
+            action_values = executor.map(
+                self._singlerun, [steps for n in range(n_runs)], list(range(n_runs))
+            )
         return np.squeeze(np.stack(list(action_values), axis=2))
 
     def output_df(self):
-        """Reshape action_values numpy array and output as pandas dataframe
-        """
-        n_rows = self.action_values.shape[2]*self.action_values.shape[0]
+        """Reshape action_values numpy array and output as pandas dataframe"""
+        n_rows = self.action_values.shape[2] * self.action_values.shape[0]
         df = pd.DataFrame(
-            data=self.action_values.transpose(2, 0, 1).reshape(-1, 4), 
-            columns=self.columns
+            data=self.action_values.transpose(2, 0, 1).reshape(-1, 4),
+            columns=self.columns,
         )
 
-        return df 
+        return df
 
 
 class EpsilonGreedy(Bandit):
     """Epsilon greedy bandit
 
     Attributes:
-        epsilon (float): 
+        epsilon (float):
             epsilon coefficient configuring the probability to explore non-optimal actions,
             ranging from 0.0 to 1.0
         alpha (float):
             Constant step size ranging from 0.0 to 1.0, resulting in Q being the weighted average
             of past rewards and initial estimate of Q
-            
+
             Note on varying step sizes such as using 1/n:
                 self.Q[self.At] = self.Q[self.At] + 1/self.nQ[self.At]*(R-self.Q[self.At])
             Theoretically guaranteed to converge, however in practice, slow to converge compared to constant alpha
@@ -145,21 +146,23 @@ class EpsilonGreedy(Bandit):
 
     def __init__(self, testbed, Q_init: Callable = init_zero, epsilon=0.1, alpha=0.1):
         super().__init__(testbed, Q_init)
-        self.epsilon = epsilon 
+        self.epsilon = epsilon
         self.alpha = alpha
-    
+
     def select_action(self):
         if np.random.binomial(1, self.epsilon) == 1:
             self.At = np.random.choice(list(self.Q.keys()))
         else:
             self.At = self.argmax(self.Q)
         R = self.testbed.action_value(self.At)
-        self.nQ[self.At] += 1 
+        self.nQ[self.At] += 1
         if self.alpha is None:
-            self.Q[self.At] = self.Q[self.At] + 1/self.nQ[self.At]*(R-self.Q[self.At])
+            self.Q[self.At] = self.Q[self.At] + 1 / self.nQ[self.At] * (
+                R - self.Q[self.At]
+            )
         else:
-            self.Q[self.At] = self.Q[self.At] + self.alpha*(R-self.Q[self.At])
-        
+            self.Q[self.At] = self.Q[self.At] + self.alpha * (R - self.Q[self.At])
+
         return (self.At, R)
 
     def output_df(self):
@@ -167,6 +170,6 @@ class EpsilonGreedy(Bandit):
         Add epsilon coefficient used for greedy bandit
         """
         df = super().output_df()
-        df['epsilon'] = self.epsilon
+        df["epsilon"] = self.epsilon
 
-        return df 
+        return df
