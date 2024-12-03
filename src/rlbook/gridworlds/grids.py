6f0957d1a4b02e5deb3e37eb51bacb2a414b3480
joseph-jnl <1985819+joseph-jnl@users.noreply.github.com>
2024-12-02 22:15:29 -0500

    add reward for states func

--- src/rlbook/gridworlds/grids.py
+++ src/rlbook/gridworlds/grids.py
@@ -1,23 +1,27 @@
 from abc import ABCMeta, abstractmethod
 
 import jax.numpy as jnp
+import jax.tree as jtree
+from jax.scipy.signal import convolve2d
+from jaxtyping import Array, Float
 
 
 class Grid(metaclass=ABCMeta):
     """ """
 
     def __init__(self, n_rows: int = 5, n_cols: int = 5, iter: int = 1000):
-        self.v = jnp.zeros(n_rows, n_cols)
+        self.v = jnp.zeros((n_rows, n_cols))
+        self.vp = jnp.zeros((n_rows, n_cols))
         self.n_rows = n_rows
         self.n_cols = n_cols
 
     @abstractmethod
-    def actions(self):
-        """ """
-
-    @abstractmethod
     def reward(self):
         """Reward for a given action"""
+
+    @abstractmethod
+    def state_value(self):
+        """"""
 
 
 class SimpleGrid(Grid):
@@ -25,33 +29,60 @@ class SimpleGrid(Grid):
 
     def __init__(
         self,
-        special_states: dict[tuple[int, int], int],
+        special_states: list[tuple[int, int]],
+        special_states_rewards: list[int],
         special_states_prime: list[tuple[int, int]],
+        actions: Float[Array, "2 4"] = jnp.array(
+            [
+                [
+                    -1,
+                    1,
+                    0,
+                    0,
+                ],
+                [0, 0, 1, -1],
+            ]
+        ),
+        actions_probs: Float[Array, "1 4"] = jnp.array([0.25, 0.25, 0.25, 0.25]),
+        n_rows: int = 5,
+        n_cols: int = 5,
+        iter: int = 1000,
     ):
         self.special_states = special_states
+        self.special_states_rewards = special_states_rewards
+        self.special_states_t = jtree.transpose(
+            jtree.structure(["*", "*"]), None, special_states
+        )
         self.special_states_prime = special_states_prime
+        self.special_states_prime_t = jtree.transpose(
+            jtree.structure(["*", "*"]), None, special_states_prime
+        )
+        self.actions = actions
+        self.actions_probs = actions_probs
 
-    def reward(self, s: tuple[int, int], sp: tuple[int, int]):
-        """Reward for a given action
-        Args:
-            s: Tuple containing row and column indices of current state
-            sp: Tuple containing row and column indices of next state moved to by the taken action
+        super().__init__(n_rows=n_rows, n_cols=n_cols, iter=iter)
 
-        """
-        condlist = [
-            # Out of bounds
-            (sp[0] == -1) | (sp[0] > (self.n_rows - 1)),
-            (sp[1] == -1) | (sp[1] > (self.n_cols - 1)),
-            # Exit special state
-            s in self.special_states,
-        ]
+        self.R = self.reward()
 
-        choicelist = [
-            # Out of bounds
-            -1,
-            -1,
-            # Exit special state
-            self.special_states[s] if s in self.special_states else 0,
-        ]
+    def policy(self):
+        """"""
 
-        return jnp.select(condlist, choicelist, default=0)  # Else return 0 as default
+        return policy
+
+    def reward(self):
+        """Provides reward for all states in grid"""
+        R = convolve2d(
+            jnp.pad(self.v, pad_width=(1, 1), constant_values=-1),
+            self.actions,
+            mode="valid",
+        )
+        R = R.at[self.special_states_t[0], self.special_states_t[1]].set(
+            self.special_states_rewards
+        )
+
+        return R
+
+    def state_value(
+        self,
+    ):
+        return 0
