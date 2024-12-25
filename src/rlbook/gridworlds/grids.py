3e2a79295c5b016cde13d3a30b0f605bf4032f58
joseph-jnl <1985819+joseph-jnl@users.noreply.github.com>
2024-12-25 13:38:01 -0500

    refactor padding to use edge, add optimal grid class

--- src/rlbook/gridworlds/grids.py
+++ src/rlbook/gridworlds/grids.py
@@ -2,16 +2,13 @@ from abc import ABCMeta, abstractmethod
 
 import jax.numpy as jnp
 from jax import jit
-from jax.scipy.signal import convolve2d
+from jax.scipy.signal import correlate2d
 from jax.tree_util import register_pytree_node_class
 from jaxtyping import Array, Float, Int
 
 
 class Grid(metaclass=ABCMeta):
-    """ 
-    
-    
-    """
+    """ """
 
     def __init__(
         self,
@@ -29,7 +26,6 @@ class Grid(metaclass=ABCMeta):
     @abstractmethod
     def policy(self): ...
 
-    @property
     @abstractmethod
     def reward(self): ...
 
@@ -81,7 +77,7 @@ class RandomGrid(Grid):
     @property
     def reward(self) -> Float[Array, "{self.n_rows} {self.n_cols}"]:
         """Provides reward for all states in grid when following a random policy"""
-        R = convolve2d(
+        R = correlate2d(
             jnp.pad(self.v_init, pad_width=(1, 1), constant_values=-1),
             self.P,
             mode="valid",
@@ -104,28 +100,16 @@ class RandomGrid(Grid):
         discount: float = 0.9,
     ) -> Float[Array, "{self.n_rows} {self.n_cols}"]:
         """"""
-        # Update interior grid
+        # Update states
         vp = (
             R
-            + convolve2d(
-                jnp.pad(v, pad_width=(1, 1), constant_values=0),
+            + correlate2d(
+                jnp.pad(v, pad_width=(1, 1), mode="edge"),
                 P,
                 mode="valid",
             )
             * discount
         )
-
-        # Update edges except for corners
-        vp = vp.at[1:-1, 0].add(v[1:-1, 0] * discount * 0.25)
-        vp = vp.at[1:-1, -1].add(v[1:-1, -1] * discount * 0.25)
-        vp = vp.at[0, 1:-1].add(v[0, 1:-1] * discount * 0.25)
-        vp = vp.at[-1, 1:-1].add(v[-1, 1:-1] * discount * 0.25)
-
-        # Update corners
-        vp = vp.at[0, 0].add(v[0, 0] * 2 * discount * 0.25)
-        vp = vp.at[0, -1].add(v[0, -1] * 2 * discount * 0.25)
-        vp = vp.at[-1, -1].add(v[-1, -1] * 2 * discount * 0.25)
-        vp = vp.at[-1, 0].add(v[-1, 0] * 2 * discount * 0.25)
 
         # Update special states
         vp = vp.at[special_states[0], special_states[1]].set(
@@ -134,6 +118,218 @@ class RandomGrid(Grid):
         )
 
         return vp
+
+    def estimate_state_value(
+        self, iter: int = 1000
+    ) -> Float[Array, "{self.n_rows} {self.n_cols}"]:
+        """"""
+        v = self.v_init
+        for _ in range(iter):
+            v = self.state_value(
+                v,
+                self.R,
+                self.P,
+                self.special_states,
+                self.special_states_prime,
+                self.special_states_rewards,
+            )
+        return v
+
+    def tree_flatten(self):
+        children = (
+            self.special_states_rewards,
+            self.R,
+            self.P,
+            self.actions,
+            self.v_init,
+        )  # arrays and dynamic values
+        # static values (non-arrays)
+        aux_data = {
+            "special_states": self.special_states,
+            "special_states_prime": self.special_states_prime,
+        }
+
+        return (children, aux_data)
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        grid = cls(
+            aux_data["special_states"],
+            aux_data["special_states_prime"],
+            children[0],
+            R=children[1],
+            P=children[2],
+        )
+        grid.v_init = children[2]
+
+        return grid
+
+
+@register_pytree_node_class
+class OptimalGrid(Grid):
+    """"""
+
+    def __init__(
+        self,
+        special_states: list[list[int, int]],
+        special_states_prime: list[list[int, int]],
+        special_states_rewards: Int[Array, "1 {len(special_states)}"],
+        n_rows: int = 5,
+        n_cols: int = 5,
+        R: Float[Array, "n_rows n_cols"] = None,
+        P: Float[Array, "3 3"] = None,
+    ):
+        super().__init__(n_rows=n_rows, n_cols=n_cols)
+        self.special_states = special_states
+        self.special_states_prime = special_states_prime
+        self.special_states_rewards = special_states_rewards
+
+        self.v_init = self.init_zeros()
+        self.P = self.policy
+        self.R = self.reward(self.v_init, self.policy)
+
+    @property
+    def policy(self) -> Float[Array, "4 3 3"]:
+        """
+        Define policy conv kernel as a 3d array
+
+        P = Array([[[0., 1., 0.], # action up only
+                    [0., 0., 0.],
+                    [0., 0., 0.]],
+
+                    [[0., 0., 0.], # action left only
+                    [1., 0., 0.],
+                    [0., 0., 0.]],
+
+                    [[0., 0., 0.], #action down only
+                    [0., 0., 0.],
+                    [0., 1., 0.]],
+
+                    [[0., 0., 0.], @action right only
+                    [0., 0., 1.],
+                    [0., 0., 0.]]]
+
+        """
+        policy = jnp.zeros((4, 3, 3))
+        policy = policy.at[[0, 1, 2, 3], [0, 1, 2, 1], [1, 0, 1, 2]].set(1)
+
+        return policy
+
+    def reward(
+        self, v: Float[Array, "{self.n_rows} {self.n_cols}"], P: Float[Array, "4 3 3"]
+    ) -> Float[Array, "{self.n_rows} {self.n_cols}"]:
+        """Provides reward for all states in grid when following an optimal policy"""
+        R = jnp.zeros((4, self.n_rows, self.n_cols))
+
+        # Policy up only
+        R = R.at[0, :, :].set(
+            correlate2d(
+                jnp.pad(v, pad_width=(1, 1), constant_values=-1),
+                P[0, :, :],
+                mode="valid",
+            )
+        )
+
+        # Policy left only
+        R = R.at[1, :, :].set(
+            correlate2d(
+                jnp.pad(v, pad_width=(1, 1), constant_values=-1),
+                P[1, :, :],
+                mode="valid",
+            )
+        )
+
+        # Policy down only
+        R = R.at[2, :, :].set(
+            correlate2d(
+                jnp.pad(v, pad_width=(1, 1), constant_values=-1),
+                P[2, :, :],
+                mode="valid",
+            )
+        )
+
+        # Policy right only
+        R = R.at[3, :, :].set(
+            correlate2d(
+                jnp.pad(v, pad_width=(1, 1), constant_values=-1),
+                P[3, :, :],
+                mode="valid",
+            )
+        )
+
+        # Set special state rewards
+        R = R.at[:, self.special_states[0], self.special_states[1]].set(
+            self.special_states_rewards
+        )
+
+        return R 
+
+    @jit
+    def state_value(
+        self,
+        v: Float[Array, "n_rows n_cols"],
+        R: Float[Array, "4 n_rows n_cols"],
+        P: Float[Array, "4 3 3"],
+        special_states: list[list[int, int]],
+        special_states_prime: list[list[int, int]],
+        special_states_rewards: Float[Array, "1 {len(special_states)}"],
+        discount: float = 0.9,
+    ) -> Float[Array, "{self.n_rows} {self.n_cols}"]:
+        """"""
+
+        vp = jnp.zeros((4, self.n_rows, self.n_cols))
+
+        # Policy up only
+        vp = vp.at[0, :, :].set(
+            R[0, :, :]
+            + correlate2d(
+                jnp.pad(v, pad_width=(1, 1), mode="edge"),
+                P[0, :, :],
+                mode="valid",
+            )
+            * discount
+        )
+
+        # Policy left only
+        vp = vp.at[1, :, :].set(
+            R[1, :, :]
+            + correlate2d(
+                jnp.pad(v, pad_width=(1, 1), mode="edge"),
+                P[1, :, :],
+                mode="valid",
+            )
+            * discount
+        )
+
+        # Policy down only
+        vp = vp.at[2, :, :].set(
+            R[2, :, :]
+            + correlate2d(
+                jnp.pad(v, pad_width=(1, 1), mode="edge"),
+                P[2, :, :],
+                mode="valid",
+            )
+            * discount
+        )
+
+        # Policy right only
+        vp = vp.at[3, :, :].set(
+            R[3, :, :]
+            + correlate2d(
+                jnp.pad(v, pad_width=(1, 1), mode="edge"),
+                P[3, :, :],
+                mode="valid",
+            )
+            * discount
+        )
+
+        # Update special states
+        vp = vp.at[:, special_states[0], special_states[1]].set(
+            v[special_states_prime[0], special_states_prime[1]] * discount
+            + special_states_rewards
+        )
+
+        return jnp.max(vp, axis=0)
 
     def estimate_state_value(
         self, iter: int = 1000
