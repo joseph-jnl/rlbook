2f55c85f29e0c073e6d190e632ea7cb8c904c963
joseph-jnl <1985819+joseph-jnl@users.noreply.github.com>
2025-03-12 22:13:44 -0400

    add initial reward broadcasting for rental car problem

---
+++ src/rlbook/gridworlds/rentals.py
@@ -0,0 +1,88 @@
+import jax.numpy as jnp
+from jax import vmap
+from jax.scipy.stats import poisson
+
+
+class Rentals:
+    """Base car rental class with jax jit related helper methods.
+
+    Attributes:
+        n_rows: number of rows.
+        n_cols: number of columns.
+        actions: actions that can be taken in the grid.
+        v_init: initial state values.
+    """
+
+    def __init__(
+        self,
+        n_rows: int = 3,
+        n_cols: int = 3,
+    ):
+        """
+        Args:
+            n_rows: number of rows.
+            n_cols: number of columns.
+        """
+        assert n_rows == n_cols  # only implement symmetrical car maximums
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+
+        self.len_idx = n_rows * n_cols
+        self.state_indices = jnp.reshape(
+            jnp.indices((n_rows, n_cols), dtype=float), (2, self.len_idx)
+        )
+
+        valid_row_rewards, valid_col_rewards = self.reward_broadcast()
+        self.R = jnp.zeros(
+            (6, n_rows, n_cols)
+        )  # max 5 cars can be moved in one night plus 0 (no action), reuse for both locations
+
+        reward_map = vmap(self.reward, in_axes=0)
+        for a in range(6):
+            self.R = self.R.at[a, :, :].set(
+                jnp.sum(
+                    reward_map(
+                        valid_row_rewards,
+                        valid_col_rewards,
+                        jnp.ones(valid_row_rewards.shape) * a,
+                    ),
+                    axis=1,
+                ).reshape((self.n_rows, self.n_cols))
+            )
+
+    def reward_broadcast(self):
+        """For each state index, broadcast the set of valid rewards,
+            using "-1" padding to mask out invalid rewards and maintain consistent array lengths
+        Note: -1 is used since this value had a zero percent chance of occuring in a poisson dist
+        """
+        valid_row_rewards = jnp.broadcast_to(
+            jnp.arange(self.n_rows), (self.len_idx, self.n_rows)
+        )
+        valid_row_rewards = jnp.where(
+            valid_row_rewards
+            > jnp.broadcast_to(self.state_indices[0], (self.n_rows, self.len_idx)).T,
+            -1,
+            valid_row_rewards,
+        )
+
+        valid_col_rewards = jnp.broadcast_to(
+            jnp.arange(self.n_cols), (self.len_idx, self.n_cols)
+        )
+        valid_col_rewards = jnp.where(
+            valid_col_rewards
+            > jnp.broadcast_to(self.state_indices[1], (self.n_cols, self.len_idx)).T,
+            -1,
+            valid_col_rewards,
+        )
+
+        return valid_row_rewards, valid_col_rewards
+
+    def reward(self, row_state_primes, col_state_primes, action):
+        p_row = poisson.pmf(row_state_primes, mu=3)
+        p_col = poisson.pmf(col_state_primes, mu=4)
+
+        return (
+            jnp.arange(p_row.shape[0]) * p_row * 10
+            + jnp.arange(p_col.shape[0]) * p_col * 10
+            - abs(action * 2)
+        )
